675462177454fab8f1bd84a8b3cd9d52aa2862b1
Tim Kamenetskiy <kamenetskiyto@gmail.com>
2024-10-05 18:53:34 +0300

    a

--- bot/handlers/candidate.py
+++ bot/handlers/candidate.py
@@ -161,7 +161,7 @@ async def handle_interview_message(message: types.Message, state: FSMContext):
     user_message = message.text
 
     # Send the initial reply message to Telegram and store its message object
-    reply_message = await message.reply("Ответ:\n")
+    reply_message = await message.reply("")
 
     full_response = ""
     last_update_time = asyncio.get_event_loop().time()
@@ -187,7 +187,7 @@ async def handle_interview_message(message: types.Message, state: FSMContext):
             current_time = asyncio.get_event_loop().time()
             if current_time - last_update_time >= 2:
                 try:
-                    await reply_message.edit_text(f"{full_response}")
+                    await reply_message.edit_text(f{full_response})
                     last_update_time = current_time  # Update the last update time
                 except aiogram.utils.exceptions.MessageNotModified:
                     pass  # Ignore if message content hasn't changed
