3b75a7b628a499c2b4407e790c8a30b6bed2247a
Tim Kamenetskiy <kamenetskiyto@gmail.com>
2024-10-05 14:44:35 +0300

    prod restart api

--- restart_api_prod.sh
+++ restart_api_prod.sh
@@ -9,24 +9,24 @@ cleanup() {
 # Trap SIGINT (Ctrl+C) and call cleanup function
 trap cleanup SIGINT
 
-echo "Rebuilding and restarting API service..."
+echo "Rebuilding and restarting API service in production..."
 
 # Stop the api service without removing the network
-sudo docker-compose -f docker-compose.yml -f docker-compose.dev.yml stop api
+sudo docker-compose -f docker-compose.yml stop api
 
 # Remove the api service container
-sudo docker-compose -f docker-compose.yml -f docker-compose.dev.yml rm -f api
+sudo docker-compose -f docker-compose.yml rm -f api
 
 # Rebuild and bring up only the api service without affecting dependencies
-sudo docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build -d --no-deps api
+sudo docker-compose -f docker-compose.yml up --build -d --no-deps api
 
 # Check the status of the service
 if [ $? -eq 0 ]; then
-    echo "API service restarted successfully. Tracking logs..."
+    echo "API service restarted successfully in production. Tracking logs..."
     
     # Start streaming the logs for the api service
-    sudo docker-compose -f docker-compose.yml -f docker-compose.dev.yml logs -f api
+    sudo docker-compose -f docker-compose.yml logs -f api
 else
-    echo "Failed to restart the API service."
+    echo "Failed to restart the API service in production."
     exit 1
 fi
